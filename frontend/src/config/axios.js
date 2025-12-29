ef258732aa614aeb4c41dd4ef89f4cd78f3f3323
Duong Huy <duonghuy121205@gmail.com>
2025-12-29 23:49:33 +0700

    test2

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://hospital-3v8p.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
