a4ec11e47abf089d76ec503375c869405770f1c5
Duong Huy <duonghuy121205@gmail.com>
2026-01-04 18:24:10 +0700

    okkk

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,7 @@ class Config:
 
     # Cấu hình Email
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
+    MAIL_PORT = 465
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USER') 
     MAIL_PASSWORD = os.environ.get('MAIL_PASS') 
